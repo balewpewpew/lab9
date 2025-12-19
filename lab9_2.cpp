691db885a0f38b7dff0f0753d9acc0fdead9f982
balewpewpew <supapitchaitan.2006@gmail.com>
2025-12-19 15:47:36 +0700

    lab9_2.cpp Completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int M,int N){
+	if(M>0&&N>0){
+		for(int i=0;i<M;i++){
+			for(int j=0;j<N;j++){
+				cout << "O";
+			}
+			cout << "\n";
+		}
+	}
+	else{
+		cout << "Invalid input";
+	}
+	
+}
 
 int main(){
 	
